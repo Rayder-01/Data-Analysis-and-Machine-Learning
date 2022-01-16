f2ab0e993d5a1440afb47b96e81d8765735550b7
Rayder <zen2362661@gmail.com>
2022-01-17 00:15:24 +0800

    27

---
+++ CH-14 ML_回歸/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print("迴歸係數", lm.coef_)
+# print("截距", lm.intercept_)
+# print(x,y)
--- CH-14 ML_回歸/回歸實際例子.py
+++ CH-14 ML_回歸/回歸實際例子.py
@@ -14,23 +14,27 @@ drink_sales = np.array([7.7, 6.2, 9.3, 8.4,
                         5.8, 9.1, 5.1, 7.3,
                         6.5, 8.4])
 
-x = pd.DataFrame(temperatures, columns=["Temperature"])
+X = pd.DataFrame(temperatures, columns=["Temperature"])
 taget = pd.DataFrame(drink_sales, columns=["Drink_sales"])
 y = taget["Drink_sales"]
 
 lm = LinearRegression()
-lm.fit(x, y)
-print("迴歸係數", lm.coef_)
-print("截距", lm.intercept_)
-print(x)
+X = X.values
+lm.fit(X, y)
+print("迴歸係數 :", lm.coef_)
+print("截距 :", lm.intercept_)
 
 #預測氣溫26, 30度的業績
-new_temperatures = pd.DataFrame()
+new_temperatures = pd.DataFrame(np.array([26,30]))
+predicted_sales = lm.predict(new_temperatures)
+print('預測氣溫26, 30度的業績 :')
+print(predicted_sales)
 
-plt.scatter(temperatures,drink_sales)
-Regression_sales = lm.predict(X)
-plt.plot(temperatures, Regression_sales, color="blue")
-plt.plot(new_temperatures, prepare_sales, color="red", marker="0", markersize=10)
+# plt.scatter(temperatures,drink_sales)
+# Regression_sales = lm.predict(X)
+# plt.plot(temperatures, Regression_sales, color="blue")
+# plt.plot(new_temperatures, prepare_sales, color="red", marker="0", markersize=10)
+
 #使用 Sklearn LinearRegression
 # 線性回歸簡單來說，就是將複雜的資料數據，擬和至一條直線上，就能方便預測未來的資料。接下來我們一樣使用房價預測資料集，並使用 Sklearn 提供的 LinearRegression 來求解。
 
